69c837d43497cb9663a64b87a06415a78e10f63a
stutiid <stutichaturvedi12@gmail.com>
2021-03-04 10:39:15 +0530

    Added: singlet combination winning percentage saved in dictionary

--- flip-combination.sh
+++ flip-combination.sh
@@ -1,10 +1,23 @@
-#welcome to flipping the coin combination and choosing the winner
 #!/bin/bash -x
 
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
-then
-	echo heads
-else
-	echo tails
-fi
+H=0
+T=0
+count=50
+
+declare -A coinCombination
+
+for ((i=1; i<=50; i++))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 0 ]
+	then
+		((H++))
+	else
+		((T++))
+	fi
+done
+
+coinCombination["H"]=$(($(($H*100))/$count))
+coinCombination["T"]=$(($T*100/count))
+
+echo ${coinCombination[@]}
