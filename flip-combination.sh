fee157961c5a55ad02786394a07f0adfea80a94e
stutiid <stutichaturvedi12@gmail.com>
2021-03-04 11:30:54 +0530

    Added: triplet combination

--- flip-combination.sh
+++ flip-combination.sh
@@ -6,6 +6,15 @@ HH=0
 HT=0
 TH=0
 TT=0
+HHH=0
+HHT=0
+HTH=0
+HTT=0
+THH=0
+THT=0
+TTH=0
+TTT=0
+
 count=60
 declare -A coinCombination
 
@@ -47,11 +56,67 @@ do
       fi
    fi
 done
-
-echo $HH $HT $TH $TT
 coinCombination["HH"]=$(($HH*100/$count))
 coinCombination["HT"]=$(($HT*100/$count))
 coinCombination["TH"]=$(($TH*100/$count))
 coinCombination["TT"]=$(($TT*100/$count))
 
+#triplet combination
+count=20
+for ((i=1; i<=count; i++))
+do
+   flip=$((RANDOM%2))
+   if [ $flip -eq 0 ]
+   then
+      flip=$((RANDOM%2))
+      if [ $flip -eq 0 ]
+      then
+         flip=$((RANDOM%2))
+			if [ $flip -eq 0 ]
+			then
+				((HHH++))
+			else
+				((HHT++))
+			fi
+		else
+          flip=$((RANDOM%2))
+         if [ $flip -eq 0 ]
+         then
+            ((HTH++))
+         else
+            ((HTT++))
+         fi
+      fi
+   else
+      flip=$((RANDOM%2))
+      if [ $flip -eq 0 ]
+      then
+          flip=$((RANDOM%2))
+         if [ $flip -eq 0 ]
+         then
+            ((THH++))
+         else
+            ((THT++))
+         fi
+      else
+          flip=$((RANDOM%2))
+         if [ $flip -eq 0 ]
+         then
+            ((TTH++))
+         else
+            ((TTT++))
+         fi
+      fi
+   fi
+done
+coinCombination["HHH"]=$(($HHH*100/$count))
+coinCombination["HHT"]=$(($HHT*100/$count))
+coinCombination["HTH"]=$(($HTH*100/$count))
+coinCombination["HTT"]=$(($HTT*100/$count))
+coinCombination["THH"]=$(($THH*100/$count))
+coinCombination["THT"]=$(($THT*100/$count))
+coinCombination["TTH"]=$(($TTH*100/$count))
+coinCombination["TTT"]=$(($TTT*100/$count))
+
 echo ${coinCombination[@]}
+
